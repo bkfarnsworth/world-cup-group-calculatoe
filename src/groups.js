379c4e33e0facc13d2fddafaab68bdfc7271ed19
bkfarnsworth <bkfarnsworth@gmail.com>
2018-06-22 08:58:57 -0600

    adding all countries

--- src/groups.js
+++ src/groups.js
@@ -1,46 +1,142 @@
 export default [
   {
-    id: "E",
-    teams: ["Brazil", "Costa Rica", "Serbia", "Switzerland"],
+    id: "A",
+    teams: ["Russia", "Egypt", "Uruguay", "Saudi Arabia"],
     matches: [
       {
-        team1: "Brazil",
-        team2: "Switzerland",
+        team1: "Russia",
+        team2: "Saudi Arabia",
         team1Score: 1,
         team2Score: 1,
         played: true
       },
       {
-        team1: "Serbia",
-        team2: "Costa Rica",
+        team1: "Uruguay",
+        team2: "Egypt",
         team1Score: 1,
         team2Score: 0,
         played: true
       },
       {
-        team1: "Brazil",
-        team2: "Serbia",
+        team1: "Russia",
+        team2: "Uruguay",
         team1Score: 0,
         team2Score: 0,
         played: false
       },
       {
-        team1: "Switzerland",
-        team2: "Costa Rica",
+        team1: "Saudi Arabia",
+        team2: "Egypt",
         team1Score: 0,
         team2Score: 0,
         played: false
       },
       {
-        team1: "Serbia",
-        team2: "Switzerland",
+        team1: "Uruguay",
+        team2: "Saudi Arabia",
         team1Score: 0,
         team2Score: 0,
         played: false
       },
       {
-        team1: "Brazil",
-        team2: "Costa Rica",
+        team1: "Russia",
+        team2: "Egypt",
+        team1Score: 2,
+        team2Score: 0,
+        played: true
+      }
+    ]
+  },
+  {
+    id: "B",
+    teams: ["Portugal", "Spain", "Iran", "Morocco"],
+    matches: [
+      {
+        team1: "Portugal",
+        team2: "Morocco",
+        team1Score: 1,
+        team2Score: 1,
+        played: true
+      },
+      {
+        team1: "Iran",
+        team2: "Spain",
+        team1Score: 1,
+        team2Score: 0,
+        played: true
+      },
+      {
+        team1: "Portugal",
+        team2: "Iran",
+        team1Score: 0,
+        team2Score: 0,
+        played: false
+      },
+      {
+        team1: "Morocco",
+        team2: "Spain",
+        team1Score: 0,
+        team2Score: 0,
+        played: false
+      },
+      {
+        team1: "Iran",
+        team2: "Morocco",
+        team1Score: 0,
+        team2Score: 0,
+        played: false
+      },
+      {
+        team1: "Portugal",
+        team2: "Spain",
+        team1Score: 2,
+        team2Score: 0,
+        played: true
+      }
+    ]
+  },
+  {
+    id: "C",
+    teams: ["France", "Peru", "Denmark", "Australia"],
+    matches: [
+      {
+        team1: "France",
+        team2: "Australia",
+        team1Score: 1,
+        team2Score: 1,
+        played: true
+      },
+      {
+        team1: "Denmark",
+        team2: "Peru",
+        team1Score: 1,
+        team2Score: 0,
+        played: true
+      },
+      {
+        team1: "France",
+        team2: "Denmark",
+        team1Score: 0,
+        team2Score: 0,
+        played: false
+      },
+      {
+        team1: "Australia",
+        team2: "Peru",
+        team1Score: 0,
+        team2Score: 0,
+        played: false
+      },
+      {
+        team1: "Denmark",
+        team2: "Australia",
+        team1Score: 0,
+        team2Score: 0,
+        played: false
+      },
+      {
+        team1: "France",
+        team2: "Peru",
         team1Score: 2,
         team2Score: 0,
         played: true
@@ -92,6 +188,198 @@ export default [
         team1Score: 0,
         team2Score: 0,
         played: false
+      }
+    ]
+  },
+  {
+    id: "E",
+    teams: ["Brazil", "Costa Rica", "Serbia", "Switzerland"],
+    matches: [
+      {
+        team1: "Brazil",
+        team2: "Switzerland",
+        team1Score: 1,
+        team2Score: 1,
+        played: true
+      },
+      {
+        team1: "Serbia",
+        team2: "Costa Rica",
+        team1Score: 1,
+        team2Score: 0,
+        played: true
+      },
+      {
+        team1: "Brazil",
+        team2: "Serbia",
+        team1Score: 0,
+        team2Score: 0,
+        played: false
+      },
+      {
+        team1: "Switzerland",
+        team2: "Costa Rica",
+        team1Score: 0,
+        team2Score: 0,
+        played: false
+      },
+      {
+        team1: "Serbia",
+        team2: "Switzerland",
+        team1Score: 0,
+        team2Score: 0,
+        played: false
+      },
+      {
+        team1: "Brazil",
+        team2: "Costa Rica",
+        team1Score: 2,
+        team2Score: 0,
+        played: true
+      }
+    ]
+  },
+  {
+    id: "F",
+    teams: ["Mexico", "Germany", "Sweden", "South Korea"],
+    matches: [
+      {
+        team1: "Mexico",
+        team2: "South Korea",
+        team1Score: 1,
+        team2Score: 1,
+        played: true
+      },
+      {
+        team1: "Sweden",
+        team2: "Germany",
+        team1Score: 1,
+        team2Score: 0,
+        played: true
+      },
+      {
+        team1: "Mexico",
+        team2: "Sweden",
+        team1Score: 0,
+        team2Score: 0,
+        played: false
+      },
+      {
+        team1: "South Korea",
+        team2: "Germany",
+        team1Score: 0,
+        team2Score: 0,
+        played: false
+      },
+      {
+        team1: "Sweden",
+        team2: "South Korea",
+        team1Score: 0,
+        team2Score: 0,
+        played: false
+      },
+      {
+        team1: "Mexico",
+        team2: "Germany",
+        team1Score: 2,
+        team2Score: 0,
+        played: true
+      }
+    ]
+  },
+  {
+    id: "G",
+    teams: ["Belgium", "England", "Tunisia", "Panama"],
+    matches: [
+      {
+        team1: "Belgium",
+        team2: "Panama",
+        team1Score: 1,
+        team2Score: 1,
+        played: true
+      },
+      {
+        team1: "Tunisia",
+        team2: "England",
+        team1Score: 1,
+        team2Score: 0,
+        played: true
+      },
+      {
+        team1: "Belgium",
+        team2: "Tunisia",
+        team1Score: 0,
+        team2Score: 0,
+        played: false
+      },
+      {
+        team1: "Panama",
+        team2: "England",
+        team1Score: 0,
+        team2Score: 0,
+        played: false
+      },
+      {
+        team1: "Tunisia",
+        team2: "Panama",
+        team1Score: 0,
+        team2Score: 0,
+        played: false
+      },
+      {
+        team1: "Belgium",
+        team2: "England",
+        team1Score: 2,
+        team2Score: 0,
+        played: true
+      }
+    ]
+  },
+  {
+    id: "H",
+    teams: ["Colombia", "Japan", "Senegal", "Poland"],
+    matches: [
+      {
+        team1: "Colombia",
+        team2: "Poland",
+        team1Score: 1,
+        team2Score: 1,
+        played: true
+      },
+      {
+        team1: "Senegal",
+        team2: "Japan",
+        team1Score: 1,
+        team2Score: 0,
+        played: true
+      },
+      {
+        team1: "Colombia",
+        team2: "Senegal",
+        team1Score: 0,
+        team2Score: 0,
+        played: false
+      },
+      {
+        team1: "Poland",
+        team2: "Japan",
+        team1Score: 0,
+        team2Score: 0,
+        played: false
+      },
+      {
+        team1: "Senegal",
+        team2: "Poland",
+        team1Score: 0,
+        team2Score: 0,
+        played: false
+      },
+      {
+        team1: "Colombia",
+        team2: "Japan",
+        team1Score: 2,
+        team2Score: 0,
+        played: true
       }
     ]
   }
