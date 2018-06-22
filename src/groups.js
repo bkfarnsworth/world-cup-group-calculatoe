3693e4359500bd245b4beda4ea3b02b258b682fe
bkfarnsworth <bkfarnsworth@gmail.com>
2018-06-21 21:16:42 -0600

    more json

--- src/groups.js
+++ src/groups.js
@@ -4,11 +4,46 @@ export default [
     teams: [],
     matches: [
       {
-        team1: "",
-        team2: "",
-        team1Score: 0,
+        team1: "Argentina",
+        team2: "Iceland",
+        team1Score: 1,
+        team2Score: 1,
+        played: true
+      },
+      {
+        team1: "Croatia",
+        team2: "Nigeria",
+        team1Score: 2,
         team2Score: 0,
         played: true
+      },
+      {
+        team1: "Argentina",
+        team2: "Croatia",
+        team1Score: 0,
+        team2Score: 3,
+        played: true
+      },
+      {
+        team1: "Iceland",
+        team2: "Nigeria",
+        team1Score: 0,
+        team2Score: 0,
+        played: false
+      },
+      {
+        team1: "Croatia",
+        team2: "Iceland",
+        team1Score: 0,
+        team2Score: 0,
+        played: false
+      },
+      {
+        team1: "Argentina",
+        team2: "Nigeria",
+        team1Score: 0,
+        team2Score: 0,
+        played: false
       }
     ]
   }
