a6e765487799ade5f512203babefc15b961106d9
bkfarnsworth <bkfarnsworth@gmail.com>
2018-06-21 21:14:44 -0600

    adding json

---
+++ src/groups.js
@@ -0,0 +1,15 @@
+export default [
+  {
+    id: "D",
+    teams: [],
+    matches: [
+      {
+        team1: "",
+        team2: "",
+        team1Score: 0,
+        team2Score: 0,
+        played: true
+      }
+    ]
+  }
+];
